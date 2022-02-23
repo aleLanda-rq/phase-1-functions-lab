93b804e404fd3727d5a2c5659fdcdf4838279fdb
Alejandra Landa <aleriquene@gmail.com>
2022-02-23 15:06:26 -0600

    commit tree 104dd1c7e67865cd108dbb837fe452c828cadc80

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+//1
+function distanceFromHqInBlocks(someValue){
+   if (someValue <42){
+       return (42-someValue);
+
+   } else {
+       return (someValue-42)
+   }
+}
+
+//2
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+//3
+function distanceTravelledInFeet(start, destination){
+    if (destination>start) {
+        return ((destination-start)*264);
+    }
+     else
+     return ((start-destination)*264);
+}
+//4
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start,destination);
+    if (distance <=400) {
+        return 0;
+    }
+    if (distance >=400 && distance<=2000) {
+        return (distance - 400) * 0.02;
+    }else if (distance >=2000 && distance <=2500){
+        return 25;
+    }
+    else if (distance >2500){
+        return "cannot travel that far";
+    }
+    }
+
+
+
